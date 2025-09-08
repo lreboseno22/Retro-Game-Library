97dec079eabbe68203a9fc5b9fd7229266da4227
Liam <77292172+lreboseno22@users.noreply.github.com>
2025-09-08 16:35:12 -0400

    little cleanup and added template engine

--- server.mjs
+++ server.mjs
@@ -1,18 +1,54 @@
 import express from "express";
+import mustacheExpress from "mustache-express";
 
 const app = express();
 const PORT = 3000;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Mustache setup
+app.engine("mustache", mustacheExpress());
+app.set("view engine", "mustache");
+app.set("views", "./src/views");
+
+// Logger
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+})
 
 let games = [
     { id: 1, title: "Super Smash Bros", platform: "Nintendo 64", year: 1999},
     { id: 2, title: "Super Mario World", platform: "Super Nintendo (SNES)", year: 1990}
 ]
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
+// ROUTES
 app.get("/", (req, res) => {
     res.send("Welcome to the Retro Game Library");
+})
+
+app.get("/library", (req, res) => {
+    res.render("index", { games });
+})
+
+app.get("/add-game", (req, res) => {
+    res.render("addGame")
+})
+
+// POST a new game
+app.post("/api/games", (req, res) => {
+    const { title, platform, year } = req.body;
+    const newGame = { 
+        id: games[games.length - 1].id + 1,
+        title,
+        platform,
+        year: parseInt(year)
+    }
+
+    games.push(newGame);
+    res.redirect("/library");
 })
 
 // GET all games
@@ -25,20 +61,6 @@ app.get("/api/games/:id", (req, res) => {
     const game = games.find(g => g.id == req.params.id);
     if(!game) return res.status(404).send("Game not Found");
     res.json(game);
-})
-
-// POST a new game
-app.post("/api/games", (req, res) => {
-    const { title, platform, year } = req.body;
-    const newGame = { 
-        id: games[games.length - 1].id + 1,
-        title,
-        platform,
-        year
-    }
-
-    games.push(newGame);
-    res.status(201).json(newGame);
 })
 
 // PUT (update) a game
@@ -68,21 +90,14 @@ app.delete("/api/games/:id", (req, res) => {
     res.status(204).send();
 })
 
-// Middleware
-// Global Error
-app.use(function(err, req, res, next){
-    res.status(500).send(err.message);
-})
-
-// Logger
-app.use((req, res) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-})
-
 // Not Found Handler
 app.use((req, res) => {
     res.status(404).send("Route not Found");
+})
+
+// Global Error
+app.use(function(err, req, res, next){
+    res.status(500).send(err.message);
 })
 
 app.listen(PORT, () => {
