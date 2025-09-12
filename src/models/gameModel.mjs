8eebdb15ac2f30e908345422cbcf42e28943b531
Liam <77292172+lreboseno22@users.noreply.github.com>
2025-09-12 15:22:31 -0400

    styling and adding more data to display

--- src/models/gameModel.mjs
+++ src/models/gameModel.mjs
@@ -1,6 +1,12 @@
 let games = [
     { id: 1, title: "Super Smash Bros", platform: "Nintendo 64", year: 1999},
-    { id: 2, title: "Super Mario World", platform: "Super Nintendo (SNES)", year: 1990}
+    { id: 2, title: "Super Mario World", platform: "SNES", year: 1990},
+    { id: 3, title: "The Legend of Zelda", platform: "NES", year: 1986 },
+    { id: 4, title: "Tetris", platform: "Game Boy", year: 1989 },
+    { id: 5, title: "Super Mario Bros.", platform: "NES", year: 1985 },
+    { id: 6, title: "Mega Man 2", platform: "NES", year: 1988 },
+    { id: 7, title: "Pokémon Red", platform: "Game Boy", year: 1996 },
+    { id: 8, title: "Donkey Kong Country", platform: "SNES", year: 1994 },
 ]
 
 export const getGames = () => games;
